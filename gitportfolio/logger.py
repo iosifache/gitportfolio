9848e7c98348f3e2a4ba96d9762b62d8f10a3832
iosifache <hi@iosifache.me>
2023-12-24 23:03:35 +0200

    Fixes the GHA

--- gitportfolio/logger.py
+++ gitportfolio/logger.py
@@ -1,10 +1,12 @@
 import logging
+import sys
 
+from github import set_log_level as pygithub_set_log_level
 from rich.logging import RichHandler
 
 FORMAT = "%(message)s"
 logging.basicConfig(
-    level="NOTSET",
+    level=logging.INFO,
     format=FORMAT,
     datefmt="[%X]",
     handlers=[RichHandler()],
@@ -17,3 +19,12 @@ def get_logger() -> logging.Logger:
 
 def disable_logger() -> None:
     get_logger().propagate = False
+
+    logging.disable(sys.maxsize)
+    logging.getLogger("requests").setLevel(sys.maxsize)
+    logging.getLogger("requests").propagate = False
+    logging.getLogger("urllib3").setLevel(sys.maxsize)
+    logging.getLogger("urllib3").propagate = False
+    logging.getLogger("requests.packages.urllib3").setLevel(sys.maxsize)
+    logging.getLogger("requests.packages.urllib3").propagate = False
+    pygithub_set_log_level(sys.maxsize)
